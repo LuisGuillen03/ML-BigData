2e023f1c1e95319bf7c69dbcf9e11aac11d7233e
enriquegomeztagle <0241823@up.edu.mx>
2025-12-01 21:51:29 -0600

    feat: Add Terraform modules for Cloud Run job, Dataproc cluster, and GCS bucket.

---
+++ infra/modules/cloud_run/main.tf
@@ -0,0 +1,23 @@
+resource "google_cloud_run_v2_job" "bronze_extract" {
+  name     = var.job_name
+  location = var.region
+  project  = var.project_id
+
+  template {
+    template {
+      containers {
+        image = var.image_url
+        
+        env {
+          name  = "PROJECT_ID"
+          value = var.project_id
+        }
+        
+        env {
+          name  = "BUCKET_NAME"
+          value = var.bucket_name
+        }
+      }
+    }
+  }
+}
