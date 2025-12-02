bcecc1b5ae8f278fdd15c639156e54d3964581f4
enriquegomeztagle <0241823@up.edu.mx>
2025-12-01 21:52:21 -0600

    feat: define storage module variables and outputs

---
+++ infra/modules/storage/outputs.tf
@@ -0,0 +1,9 @@
+output "bucket_name" {
+  description = "GCS bucket name"
+  value       = google_storage_bucket.medallion_bucket.name
+}
+
+output "bucket_url" {
+  description = "GCS bucket URL"
+  value       = google_storage_bucket.medallion_bucket.url
+}
